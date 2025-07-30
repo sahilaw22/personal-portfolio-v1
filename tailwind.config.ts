7180f9994b443e583e4e248197a8f9c8d0bf2813
Sahil Ahmed Wani <sahilaw502@gmail.com>
2025-07-30 01:49:29 +0000

    Use different font to write my name Sahil Ahmed Wani

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,7 +18,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Space Grotesk"', 'sans-serif'],
-        headline: ['"Orbitron"', 'sans-serif'],
+        headline: ['"Audiowide"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
