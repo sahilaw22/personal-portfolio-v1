f9572dd910fb8e802e73fc3b341c00891632a9cb
Sahil Ahmed Wani <sahilaw502@gmail.com>
2025-07-30 14:34:58 +0000

    Make "Get in touch" and "Send message" button in this style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        accent: "bg-accent text-accent-foreground hover:bg-accent/90",
+        accent: "rounded-full border-2 border-primary-foreground bg-accent text-accent-foreground shadow-lg hover:bg-accent/90",
       },
       size: {
         default: "h-10 px-4 py-2",
