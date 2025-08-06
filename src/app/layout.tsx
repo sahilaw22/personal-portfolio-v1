caeca4408db29354fdd877718e4425688055ef11
Sahil Ahmed Wani <sahilaw502@gmail.com>
2025-08-06 18:53:35 +0000

    Take this image as reference add multiple new fonts . I want around all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Space+Grotesk:wght@300..700&family=Roboto+Slab:wght@400;700&family=Playfair+Display:wght@400;700&family=Inter:wght@400;700&family=Lexend:wght@400;700&family=Source+Code+Pro:wght@400;700&family=JetBrains+Mono:wght@400;700&family=IBM+Plex+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Space+Grotesk:wght@300..700&family=Roboto+Slab:wght@400;700&family=Playfair+Display:wght@400;700&family=Inter:wght@400;700&family=Lexend:wght@400;700&family=Source+Code+Pro:wght@400;700&family=JetBrains+Mono:wght@400;700&family=IBM+Plex+Mono:wght@400;700&family=Lato:wght@400;700&family=Montserrat:wght@400;700&family=Oswald:wght@400;700&family=Raleway:wght@400;700&family=Merriweather:wght@400;700&family=PT+Sans:wght@400;700&family=Open+Sans:wght@400;700&family=Nunito:wght@400;700&family=Ubuntu:wght@400;700&family=Roboto:wght@400;700&family=Zilla+Slab:wght@400;700&family=Domine:wght@400;700&family=Cormorant+Garamond:wght@400;700&family=EB+Garamond:wght@400;700&family=Libre+Baskerville:wght@400;700&family=Lora:wght@400;700&family=Playbill&family=Lobster&family=Pacifico&family=Anton&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <AppStateProvider>
