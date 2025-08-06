1f6f602359179dfb2b5367cc663e9f5de9cbe760
Sahil Ahmed Wani <sahilaw502@gmail.com>
2025-08-06 13:33:04 +0000

    Great, now same after for all buttons which following the same onclick c

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,16 +10,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground",
         outline:
           "border border-input bg-background",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
-        accent: "border-2 border-primary-foreground bg-accent text-accent-foreground shadow-lg hover:bg-accent/90",
+          "bg-secondary text-secondary-foreground",
+        ghost: "",
+        link: "text-primary underline-offset-4",
+        accent: "border-2 border-primary-foreground bg-accent text-accent-foreground shadow-lg",
       },
       size: {
         default: "h-10 px-4 py-2",
